9b6992545903984db783377b7f3b2f41a1a0a9ef
liusuxian <382185882@qq.com>
2022-07-25 11:29:00 +0800

    update

--- src/store/modules/download.js
+++ src/store/modules/download.js
@@ -5,7 +5,7 @@ const state = {
 const mutations = {
   // 修改进度列表
   SET_PROGRESS: (state, progressObj) => {
-    // 前面说的path时间戳是唯一存在的，所以如果在进度列表中找到当前的进度对象
+    // path时间戳是唯一存在的
     var index = state.progressList.findIndex(item => item.path === progressObj.path)
     if (index !== -1) {
       state.progressList[index].progress = progressObj.progress
@@ -14,8 +14,8 @@ const mutations = {
     }
   },
   // 删除进度列表中的进度对象
-  DEL_PROGRESS: (state, props) => {
-    state.progressList.splice(state.progressList.findIndex(item => item.path === props), 1)
+  DEL_PROGRESS: (state, path) => {
+    state.progressList.splice(state.progressList.findIndex(item => item.path === path), 1)
   }
 }
 
@@ -27,8 +27,8 @@ const actions = {
   },
   delProgress({
     commit
-  }, props) {
-    commit('DEL_PROGRESS', props)
+  }, path) {
+    commit('DEL_PROGRESS', path)
   }
 }
 
